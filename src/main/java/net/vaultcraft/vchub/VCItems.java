fe73d47bf3e334277a8fe9afd6e1c90828ad8649
Kai Mills <craftfestnetwork@gmail.com>
2014-10-09 21:28:26 -0600

    Fixed help item colors, again.

--- src/main/java/net/vaultcraft/vchub/VCItems.java
+++ src/main/java/net/vaultcraft/vchub/VCItems.java
@@ -19,7 +19,7 @@ public class VCItems {
     public static final ItemStack GAME_SELECTOR = build(Material.COMPASS, "&d&lGame Selector");
     public static final ItemStack STORE = build(Material.DIAMOND, "&b&lServer Store");
     public static final ItemStack SPEED_BOOST = build(Material.POTION, (byte)8226, "&a&lSpeed Boost");
-    public static final ItemStack NO_ACTIVE_PERK = build(Material.BUCKET, "&c&l&nNo Active Perk!", "&5Unlock perks by donating today!", "&5Use &e\"/buy\"&5 for more information.");
+    public static final ItemStack NO_ACTIVE_PERK = build(Material.BUCKET, "&c&l&nNo active perk!", "&5Unlock perks by donating today!", "&5Use &e\"/buy\"&5 for more information.");
     public static final ItemStack PICK_PERK = build(Material.BEACON, "&b&lSelect Perk");
     public static final ItemStack VAULT_COINS = build(Material.EMERALD, "&6&lTotal Tokens: &f{0}", "&5Buy more tokens today.", "Use &e\"/buy\"&5 for more information.");
     public static final ItemStack PEARL_OF_TELEPORTATION = build(Material.ENDER_PEARL, "&5&l&nPearl of Teleportation");
@@ -36,10 +36,10 @@ public class VCItems {
     public static final ItemStack SPEED_BOOST_TUTORIAL = build(Material.POTION, (byte)8226, "&a&lSpeed Boost", "&dThis is the &a&lSpeed Boost&d. It", "&dmakes you run faster around", "&dthe hub!");
     public static final ItemStack NO_ACTIVE_PERK_TUTORIAL = build(Material.BUCKET, "&c&l&nNo active perk!", "&dThis is the perk slot. If", "&dyou have a perk active, it", "&dwill show up here.");
     public static final ItemStack PICK_PERK_TUTORIAL = build(Material.BEACON, "&b&lSelect Perk", "&dThis is the perk menu. It", "&dopens a GUI filled with our", "&dhub perks. Purchase perks at", "&6store.vaultcraft.net&d!");
-    public static final ItemStack VAULT_COINS_TUTORIAL = build(Material.EMERALD, "&6&lTotal tokens:", "&dThis item shows your total", "&damount of tokens. Purchase tokens", "&dat &6store.vaultcraft.net&d!");
+    public static final ItemStack VAULT_COINS_TUTORIAL = build(Material.EMERALD, "&6&lTotal Tokens:", "&dThis item shows your total", "&damount of tokens. Purchase tokens", "&dat &6store.vaultcraft.net&d!");
     public static final ItemStack PEARL_OF_TELEPORTATION_TUTORIAL = build(Material.ENDER_PEARL, "&5&l&nPearl of Teleportation", "&dThis item is the &5&l&nPearl of", "&5&l&nTeleportation&d. Throw it and", "&dsee what happens!");
     public static final ItemStack MAP_BY_TUTORIAL = build(Material.PAPER, "&5&lMap by: &7&n4thewar", "&dThis item displays our builder,", "&64thewar. &dSay hello if you see", "&dhim in game!");
-    public static final ItemStack USER_SETTINGS_TUTORIAL = build(Material.REDSTONE_TORCH_ON, "&c&lUser settings...", "&dThis is the &c&lUser settings", "&dmenu. It opens your personal", "&dsettings that you can edit!");
+    public static final ItemStack USER_SETTINGS_TUTORIAL = build(Material.REDSTONE_TORCH_ON, "&c&lUser Settings...", "&dThis is the &c&lUser settings", "&dmenu. It opens your personal", "&dsettings that you can edit!");
 
     public static ItemStack build(Material type, String displayName, String... lore) {
         return build(type, (byte)0, displayName, lore);
