205098e086721674aa8bec0d5f4c7840e5d0f200
Nicholas <tacticalsk8er@gmail.com>
2014-10-11 15:13:03 -0700

    Updated scoreboard

--- src/main/java/net/vaultcraft/vchub/task/scoreboard/VCScoreboardController.java
+++ src/main/java/net/vaultcraft/vchub/task/scoreboard/VCScoreboardController.java
@@ -1,6 +1,5 @@
 package net.vaultcraft.vchub.task.scoreboard;
 
-import net.vaultcraft.vchub.VCHub;
 import net.vaultcraft.vchub.perks.PerkHandler;
 import net.vaultcraft.vcutils.scoreboard.*;
 import net.vaultcraft.vcutils.user.User;
@@ -9,7 +8,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
-import java.util.List;
 
 /**
  * Created by Connor on 9/2/14. Designed for the VCHub project.
@@ -27,12 +25,10 @@ public class VCScoreboardController implements Runnable {
 
         header = new VCTicker(ChatColor.BOLD, "Welcome "+player.getName()+" to VaultCraft!               ", 14);
 
-        text.put(15, "----------------");
-        text.put(14, "&5&lTokens");
-        text.put(13, "{tokens}");
-        text.put(12, " ");
-        text.put(11, "&5&lOnline Staff");
-        text.put(9, "  ");
+        text.put(12, "----------------");
+        text.put(11, "&5&lTokens");
+        text.put(10, "{tokens}");
+        text.put(9, " ");
         text.put(8, "&5&lOnline");
         text.put(7, "{online}");
         text.put(6, "   ");
@@ -40,20 +36,15 @@ public class VCScoreboardController implements Runnable {
         text.put(4, "{perk}");
         text.put(3, "    ");
         text.put(2, "&5&lWebsite");
+        text.put(1, "vaultcraft.net");
     }
 
     private VCTicker header;
-    private VCTicker staff = new VCTicker("", 16);
-    private VCTicker website = new VCTicker("https://vaultcraft.net               ", 16);
-
     private HashMap<Integer, String> text = new HashMap<>();
 
     public void run() {
         if (current == null) {
             current = new VCObjective(header.tick());
-
-            current.addScore(new VCScore(format(VCHub.getStaff()), 10, current));
-            current.addScore(new VCScore("", 1, current));
 
             for (int x : text.keySet()) {
                 String txt = text.get(x);
@@ -65,12 +56,6 @@ public class VCScoreboardController implements Runnable {
 
         current.setName(header.tick());
 
-        try {
-            staff.updateTicker(format(VCHub.getStaff())+"               ");
-            current.getFirstScore(1).setName(website.tick());
-            current.getFirstScore(10).setName(staff.tick());
-        } catch (IndexOutOfBoundsException ex) { }
-
         for (int x : text.keySet()) {
             VCScore score = current.getFirstScore(x);
             String use = text.get(x);
@@ -79,16 +64,5 @@ public class VCScoreboardController implements Runnable {
             use = use.replace("{tokens}", User.fromPlayer(player).getTokens()+"");
             score.setName(ChatColor.translateAlternateColorCodes('&', use));
         }
-    }
-
-    public static String format(List<Player> players) {
-        String s = "";
-        for (Player p : players) {
-            s+=p.getName()+", ";
-        }
-        if (s.endsWith(", "))
-            s = s.substring(0, s.length()-2);
-
-        return s;
     }
 }
