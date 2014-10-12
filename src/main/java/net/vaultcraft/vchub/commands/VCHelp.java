9c5883b636c46178290373524818d281a8e2d739
Kai Mills <craftfestnetwork@gmail.com>
2014-10-12 09:24:52 -0600

    Updated hub help menu.

--- src/main/java/net/vaultcraft/vchub/commands/VCHelp.java
+++ src/main/java/net/vaultcraft/vchub/commands/VCHelp.java
@@ -16,7 +16,7 @@ import org.bukkit.inventory.ItemStack;
 
 public class VCHelp extends ICommand {
 
-    public static Inventory help = Bukkit.createInventory(null, 27, "VaultCraft - Help Menu");
+    public static Inventory help = Bukkit.createInventory(null, 9, "VaultCraft - Help Menu");
 
     public VCHelp(String name, Group permission, String... aliases) {
         super(name, permission, aliases);
@@ -24,18 +24,15 @@ public class VCHelp extends ICommand {
 
     @Override
     public void processCommand(Player player, String[] strings) {
-        help.setItem(2, new ItemStack(VCItems.WEBSITE));
-        help.setItem(4, new ItemStack(VCItems.WELCOME));
-        help.setItem(6, new ItemStack(VCItems.SHOP));
-        help.setItem(18, new ItemStack(VCItems.GAME_SELECTOR_TUTORIAL));
-        help.setItem(19, new ItemStack(VCItems.STORE_TUTORIAL));
-        help.setItem(20, new ItemStack(VCItems.SPEED_BOOST_TUTORIAL));
-        help.setItem(21, new ItemStack(VCItems.NO_ACTIVE_PERK_TUTORIAL));
-        help.setItem(22, new ItemStack(VCItems.PICK_PERK_TUTORIAL));
-        help.setItem(23, new ItemStack(VCItems.VAULT_COINS_TUTORIAL));
-        help.setItem(24, new ItemStack(VCItems.PEARL_OF_TELEPORTATION_TUTORIAL));
-        help.setItem(25, new ItemStack(VCItems.MAP_BY_TUTORIAL));
-        help.setItem(26, new ItemStack(VCItems.USER_SETTINGS_TUTORIAL));
+        help.setItem(0, new ItemStack(VCItems.GAME_SELECTOR_TUTORIAL));
+        help.setItem(1, new ItemStack(VCItems.STORE_TUTORIAL));
+        help.setItem(2, new ItemStack(VCItems.SPEED_BOOST_TUTORIAL));
+        help.setItem(3, new ItemStack(VCItems.NO_ACTIVE_PERK_TUTORIAL));
+        help.setItem(4, new ItemStack(VCItems.PICK_PERK_TUTORIAL));
+        help.setItem(5, new ItemStack(VCItems.VAULT_COINS_TUTORIAL));
+        help.setItem(6, new ItemStack(VCItems.PEARL_OF_TELEPORTATION_TUTORIAL));
+        help.setItem(7, new ItemStack(VCItems.MAP_BY_TUTORIAL));
+        help.setItem(8, new ItemStack(VCItems.USER_SETTINGS_TUTORIAL));
         player.openInventory(help);
     }
 
